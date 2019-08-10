8b7613189ca64b3f9f9fdf028c52a101a0616cba
cyX_GUG <33390810+chenyangcyx@users.noreply.github.com>
2019-08-10 16:43:24 +0800

    Update 2019-08-10 16:41
    更新：
    1、缩短了数据的查询间隔，使得单片机有更快的反应

--- src/Global/ConnectPara.java
+++ src/Global/ConnectPara.java
@@ -7,7 +7,7 @@ public class ConnectPara
 	//本服务器端口
 	public final int server_port=5000;
 	//查询要发送的信息的时间间隔
-	public final int check_interval=1000;
+	public final int check_interval=200;
 	//接收线程超时的时间
 	public final long thread_timeout_time=5*60*1000;
 }
