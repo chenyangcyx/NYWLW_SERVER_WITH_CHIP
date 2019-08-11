11ab6a5490aee65f4479795addab8dd3aaa4f70d
cyX_GUG <33390810+chenyangcyx@users.noreply.github.com>
2019-08-11 14:20:53 +0800

    Update 2019-08-11 14:20
    更新：
    1、删除了可能导致线程混乱的代码
    2、更改了MySQL配置，修正了因为连接等待超时出现的拒绝服务

--- src/Handle/ReceiveMessage.java
+++ src/Handle/ReceiveMessage.java
@@ -30,7 +30,6 @@ public class ReceiveMessage extends Thread
 		this.thread_creating_time=Utils.utils.GetTimeByLong();
 	}
 	
-	@SuppressWarnings("deprecation")
 	public void run()
 	{
 		String str=null;
@@ -47,26 +46,6 @@ public class ReceiveMessage extends Thread
 				if(Utils.utils.AnalyzeMessage(str, ds))
 					//将设置好的消息写入数据库
 					Utils.utils.WriteDataMessage(ds);
-				//消息为无用消息
-				else
-				{
-					//发出通知
-					Utils.utils.SendSystemMessage("消息为无用消息，立即关闭接收与发送线程！剩余"+(Utils.utils.all_receive_thread.size()-1)+"个线程！");
-					
-					//关闭发送线程的Socket
-					Utils.utils.all_send_thread.get(Utils.utils.all_receive_thread.indexOf(this)).so.close();
-					//从List中移除发送线程
-					Utils.utils.all_send_thread.remove(Utils.utils.all_receive_thread.indexOf(this));
-					//关闭发送线程
-					Utils.utils.all_send_thread.get(Utils.utils.all_receive_thread.indexOf(this)).stop();
-					
-					//关闭本线程的Socket
-					this.so.close();
-					//从List中移除接收线程
-					Utils.utils.all_receive_thread.remove(this);
-					//关闭本线程
-					this.stop();
-				}
 				//刷新当前线程的记录时间
 				RefreshTime();
 			}
